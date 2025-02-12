deb0351188ca87e6c1cc124212d4c06d20bffa61
xStOpx <gle2012za@hotmail.com>
2025-02-12 19:55:55 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Valentine from "./components/Valentine";
-import Test from "./components/test";
 import ValentineBook from "./gallerymemory/page";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
    
    <div>
       <Valentine />
-      <Test />
       <ValentineBook />
    </div>
   );
