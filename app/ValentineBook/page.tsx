3abbc523d4b75d5ae53637d91cdb913628c8316d
xStOpx <gle2012za@hotmail.com>
2025-02-13 16:53:12 +0700

    Update page.tsx

--- app/ValentineBook/page.tsx
+++ app/ValentineBook/page.tsx
@@ -50,7 +50,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339162086739021875/005.jpg?ex=67adb761&is=67ac65e1&hm=c8a3ab934bcf6a7669590e1944ddb78997c5433dc0fa5fdfbd60977b2e786b13&=&format=webp&width=525&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-3">
@@ -63,7 +63,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339162087359774770/003.jpg?ex=67adb762&is=67ac65e2&hm=b4bcab359127b0818cee251de4f1e8df918615b822083bd96372b32229f560cb&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -76,7 +76,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339162087359774770/003.jpg?ex=67adb762&is=67ac65e2&hm=b4bcab359127b0818cee251de4f1e8df918615b822083bd96372b32229f560cb&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -89,7 +89,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339162088169410603/001.jpg?ex=67adb762&is=67ac65e2&hm=d74c619ddec9c57d8f55768427629f07010e8dd343c0c8f3b29f3ef23b3d48e9&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -102,14 +102,14 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339162088509014048/009.jpg?ex=67adb762&is=67ac65e2&hm=e96afbfe8173416e2ede95146f06ab036b012d801308d9119419dcc7014dcf61&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
 
               {showModal && (
                 <dialog className="modal modal-open">
                   <div className="modal-box max-w-5xl">
-                    <img src={selectedImage} alt="Ruengjarat" className="w-full h-auto" />
+                    <img src={selectedImage}  className="w-full h-auto" />
                     <div className="modal-action">
                       <button
                         className="btn"
@@ -230,7 +230,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339488940419452988/photo_--_--_2.jpg?ex=67aee7c9&is=67ad9649&hm=16b6e66f6079875b33337c10e1b0aa9ec735ead146cc1bfd03dd5a64c2b0efb3&=&format=webp&width=526&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-3">
@@ -243,7 +243,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339488939257626635/photo_--_--_2.jpg?ex=67aee7c9&is=67ad9649&hm=2a9c50ea458f9eb4c6536207f4288a0d84a50a103d68d8b8dadcf0a4905fb3c7&=&format=webp&width=526&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -256,7 +256,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339488939903287318/photo_--_--.jpg?ex=67aee7c9&is=67ad9649&hm=d4ddb355cf3afebd56b2eecb750ca9800c850153f1a0f0f9bcd8a5ba1b6f2636&=&format=webp&width=526&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -269,7 +269,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339488939559489556/photo_--_--.jpg?ex=67aee7c9&is=67ad9649&hm=45797523a0db892ef299119fc414504505e3e672fb9bf8a8f2effb22b5ba387e&=&format=webp&width=526&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -282,14 +282,14 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339488940754866237/photo_--_--.jpg?ex=67aee7c9&is=67ad9649&hm=b6cd568725c077de16e20d4579db2baabc1857adcaea222c312b3e8ed6c807ef&=&format=webp&width=526&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
 
               {showModal && (
                 <dialog className="modal modal-open">
                   <div className="modal-box max-w-5xl">
-                    <img src={selectedImage} alt="Ruengjarat" className="w-full h-auto" />
+                    <img src={selectedImage}  className="w-full h-auto" />
                     <div className="modal-action">
                       <button
                         className="btn"
@@ -411,7 +411,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339487755415195728/photo_--_--_2.jpg?ex=67aee6af&is=67ad952f&hm=73c3ccaa79bb95c7964ae4ab6e401fcb4d46f8b42abf20c3bcf26bd514b29675&=&format=webp&width=526&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-3">
@@ -424,7 +424,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339487755788353578/photo_--_--.jpg?ex=67aee6af&is=67ad952f&hm=985a7f774052a5df2ea62f70fd41a62ed2e793d1f22fe4e04d8724f2e0a9c637&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -437,7 +437,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339487756778340362/photo_--_--_3.jpg?ex=67aee6af&is=67ad952f&hm=97fd4431a4c27ae4a0b0a914aad0d1e5172037830ae23f921f2007fa30bd24fb&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -450,7 +450,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339487756073701436/photo_--_--.jpg?ex=67aee6af&is=67ad952f&hm=a492b86ed8784d2be55829be6d2aa64c51e46f539f3c60b8c0bc908d4c1f3a11&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -463,14 +463,14 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339487756383944765/photo_--_--.jpg?ex=67aee6af&is=67ad952f&hm=6c18313b2019ecaaa398e2e1334e576b3581e56e02da4845d9a01169ca66681e&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
 
               {showModal && (
                 <dialog className="modal modal-open">
                   <div className="modal-box max-w-5xl">
-                    <img src={selectedImage} alt="Ruengjarat" className="w-full h-auto" />
+                    <img src={selectedImage}  className="w-full h-auto" />
                     <div className="modal-action">
                       <button
                         className="btn"
@@ -592,7 +592,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339485432622219284/photo_--_--_2.jpg?ex=67aee485&is=67ad9305&hm=bbc5bfa1034ebad95abba6f67fecc258001d7b208aace9641aba5979377e7090&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-3">
@@ -605,7 +605,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339485433058295849/photo_--_--.jpg?ex=67aee485&is=67ad9305&hm=a7bc7e1dae33039556a106204c0447fd950dc04ce6aeea648870e2d0a4ce06b1&=&format=webp&width=525&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -618,7 +618,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339485433398300742/photo_--_--.jpg?ex=67aee485&is=67ad9305&hm=dd8a93887ed14d5f1d984a9f3b1b06c2e37bad48ba367e0f8e3fef654fc44625&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -631,7 +631,7 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339485433792299128/photo_--_--.jpg?ex=67aee485&is=67ad9305&hm=9c26bc2f7aeec435cbc505cdca3077cef21eab5c1d1a49c3802c44f663659684&=&format=webp&width=394&height=700"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
               <div className="col-span-2">
@@ -644,14 +644,14 @@ export default function ValentineBook() {
                     setShowModal(true);
                   }}
                   src="https://media.discordapp.net/attachments/1283830529933840507/1339485434215927808/photo_--_--.jpg?ex=67aee485&is=67ad9305&hm=c13bdb6ac8708b12ab19a641195f1062949dd7dee45e4f0d1f0c979107f23cd9&=&format=webp&width=526&height=701"
-                  alt="Ruengjarat"
+                  
                 />
               </div>
 
               {showModal && (
                 <dialog className="modal modal-open">
                   <div className="modal-box max-w-5xl">
-                    <img src={selectedImage} alt="Ruengjarat" className="w-full h-auto" />
+                    <img src={selectedImage}  className="w-full h-auto" />
                     <div className="modal-action">
                       <button
                         className="btn"
